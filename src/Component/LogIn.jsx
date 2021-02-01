72d6aac6d7d1a5970b3f84104e1ed47748b408d3
poushalibhatt <poume.94@gmail.com>
2021-02-01 18:56:47 +0530

    Added error message

--- src/Component/LogIn.jsx
+++ src/Component/LogIn.jsx
@@ -1,13 +1,20 @@
 import React,{useState} from 'react';
 import {useHistory} from 'react-router-dom';
+import {
+    Formik,
+    Form,
+    ErrorMessage
+    } from 'formik';
 
 let LogIn=()=>{
     const[name, setName]=useState('');
     const[password, setPassword]=useState('');
     const history= useHistory();
-
-    let handleChange=(e)=>{
+    
+    let handleNameChange=(e)=>{
         setName(e.target.value);
+    }
+    let handlePasswordChange=(e)=>{
         setPassword(e.target.value);
     }
     let handleClick=(e)=>{
@@ -15,13 +22,19 @@ let LogIn=()=>{
         history.push()
     }
     return(
-        <form>
-            <label>User Name</label>
-            <input type="text" value={name} onChange={handleChange}/>
-            <label>Password</label>
-            <input type="password" value={password} onChange={handleChange}/>
-            <button type='submit' onClick={handleClick}>LogIn</button>
-        </form>
+        <Formik>
+            
+         
+            <Form>
+                <label htmlFor='name'>User Name</label>
+                <input type="text" value={name} name='name' onChange={handleNameChange}/>
+                <ErrorMessage name='name' />
+                <label htmlFor='password'>Password</label>
+                <input type="password" value={password} name='password' onChange={handlePasswordChange}/>
+                <ErrorMessage name='password' />
+                <button type='submit' onClick={handleClick}>LogIn</button>
+            </Form>
+        </Formik>
     )
 }
 
